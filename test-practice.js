74dc2b40b07d6b57253843bc72b12607f40c04b2
Thomas <tho.daskalakis@gmail.com>
2022-11-25 00:09:04 +0200

    Implement reverseString

--- test-practice.js
+++ test-practice.js
@@ -2,6 +2,17 @@ function capitalizeString(string) {
   return string.at(0).toUpperCase() + string.slice(1);
 }
 
+function reverseString(string) {
+  let myString = '';
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    myString += string[i];
+  }
+
+  return myString;
+}
+
 module.exports = {
   capitalizeString,
+  reverseString,
 };
--- test-practice.test.js
+++ test-practice.test.js
@@ -3,3 +3,7 @@ const practice = require('./test-practice');
 test('capitalize string', () => {
   expect(practice.capitalizeString('mY strINg')).toBe('MY strINg');
 });
+
+test('reverse string', () => {
+  expect(practice.reverseString('reversed?')).toBe('?desrever');
+});
